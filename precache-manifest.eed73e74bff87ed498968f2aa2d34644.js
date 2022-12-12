3d840bd8f9e7e44f5faa18214bb201e6f2308984
HIilLm <97495105+HIilLm@users.noreply.github.com>
2022-12-12 22:42:08 +0700

    cc

---
+++ precache-manifest.eed73e74bff87ed498968f2aa2d34644.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb0fb6f369131c9345d",
+    "url": "/css/app.a58180b7.css"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/favicosan.ico"
+  },
+  {
+    "revision": "60dc2a205c9bbbe745455315b135707b",
+    "url": "/img/bacground website-01.png"
+  },
+  {
+    "revision": "a75bd29884d14fb02cfb9e72b21154a5",
+    "url": "/img/background.jpg"
+  },
+  {
+    "revision": "3fb4d6e148c026a1f59ab701a1adbd2e",
+    "url": "/img/bg rev.jpg"
+  },
+  {
+    "revision": "19ff94ecd2b6b0cfc1840ed38ab3dea1",
+    "url": "/img/c.jpg"
+  },
+  {
+    "revision": "ad51f5b9d88a8177e70cd3824b5978e2",
+    "url": "/img/f.jpg"
+  },
+  {
+    "revision": "d63c50d91df557a6fc5f0cd60e8a4ea2",
+    "url": "/img/g-0.jpg"
+  },
+  {
+    "revision": "9705002b2a3bdfba70f78784532c94d1",
+    "url": "/img/h.jpeg"
+  },
+  {
+    "revision": "259d030256f852804fc18c40ed583df1",
+    "url": "/img/pages/01.jpg"
+  },
+  {
+    "revision": "8060579e11d7bf8265f805ca4c7f2b39",
+    "url": "/img/pages/04.jpg"
+  },
+  {
+    "revision": "89e7b2d0ba82c736a1f51761f03b5739",
+    "url": "/img/pages/05.jpg"
+  },
+  {
+    "revision": "a903f7a5d036f505fdabbcacd6a38386",
+    "url": "/img/pages/06.jpg"
+  },
+  {
+    "revision": "a6f1d0ee3d842d47ec318b957a2e4e93",
+    "url": "/img/pages/07.jpg"
+  },
+  {
+    "revision": "419aa2a19aca5c6e5d1027a1823833d2",
+    "url": "/img/pages/11.jpg"
+  },
+  {
+    "revision": "65e6913028b5db686a5c302c0f8b0199",
+    "url": "/img/pages/12.jpg"
+  },
+  {
+    "revision": "abbaecde70532afa47b560ec1c8ffb06",
+    "url": "/img/pages/g-01.jpg"
+  },
+  {
+    "revision": "bde643dddd9dbe1cea442aaf4d18bcc0",
+    "url": "/img/pages/g-02.jpg"
+  },
+  {
+    "revision": "0a70b87e7eca747d9e4cb4e8510f66d3",
+    "url": "/img/pages/g-03.jpg"
+  },
+  {
+    "revision": "8c1a407712404dfca42950de4cf5d0fa",
+    "url": "/img/pages/g-04.jpg"
+  },
+  {
+    "revision": "73677097a2c23404c1bf2ba2abd0fab3",
+    "url": "/img/pages/g-05.jpg"
+  },
+  {
+    "revision": "dc1e65f4516c02b0dd34aa6e7a7c91a2",
+    "url": "/img/pages/g-06.jpg"
+  },
+  {
+    "revision": "0217bd87059ec3c094d12995386062dd",
+    "url": "/img/pages/g-07.jpg"
+  },
+  {
+    "revision": "bab3c86da10346adac5da4898be25733",
+    "url": "/img/pages/g-08.jpg"
+  },
+  {
+    "revision": "cae75e1184512c118999bf424759db10",
+    "url": "/img/pages/g-09.jpg"
+  },
+  {
+    "revision": "48e60666ac7c133f98709f03278d1f84",
+    "url": "/img/pages/g-10.jpg"
+  },
+  {
+    "revision": "1af3ed001ad111b0e75df16ecb286a6f",
+    "url": "/img/pages/g-11.jpg"
+  },
+  {
+    "revision": "862876953637cd74a74bef0c620c848a",
+    "url": "/img/pages/g-12.jpg"
+  },
+  {
+    "revision": "6b7b3149a1d867976c603200490ce435",
+    "url": "/img/pages/g-13.jpg"
+  },
+  {
+    "revision": "a3702e2b2f1477b1007f87d54b8de293",
+    "url": "/img/pages/g-14.jpg"
+  },
+  {
+    "revision": "c850376086e67dc49ed4d8f09bad9594",
+    "url": "/img/pages/g-15.jpg"
+  },
+  {
+    "revision": "e10c08da833bde06d73e3565a308fdc8",
+    "url": "/img/pages/g-16.jpg"
+  },
+  {
+    "revision": "0794e6fecbc69f42e8d42d6293bd36b6",
+    "url": "/img/pages/g-17.jpg"
+  },
+  {
+    "revision": "d79242edcb6e207ff299b3346f41973a",
+    "url": "/img/w.jpeg"
+  },
+  {
+    "revision": "4cbf48f5017eca092b3bd6076f65bdec",
+    "url": "/img/z.jpg"
+  },
+  {
+    "revision": "1679f2043a26186866f4c1d29d2532f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eb0fb6f369131c9345d",
+    "url": "/js/app.5fba2eb2.js"
+  },
+  {
+    "revision": "1f7d24c889c19d038a42",
+    "url": "/js/chunk-vendors.c8aac5ce.js"
+  },
+  {
+    "revision": "4e0ff27224983f096937849b6496e7db",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "75a5a1d096ddf1effb86d1b1ce96c67f",
+    "url": "/video/cover.mp4"
+  },
+  {
+    "revision": "edf9f93f396ec59b18400d2421aab670",
+    "url": "/video/cover.ogv"
+  },
+  {
+    "revision": "6e3bff11b0505c506b6d7af4315f9093",
+    "url": "/video/cover.webm"
+  }
+]);
